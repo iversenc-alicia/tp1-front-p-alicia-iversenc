9db5645a9cc368c7c3a835d4fff4e6723ce58a53
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-09-09 11:31:02 +0200

    cration page liste

---
+++ src/pages/liste.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="p-2">
+        <h1 class="text-2xl">Page Liste</h1>
+        <div v-for="maisons in maisons" :key="maisons.nom">
+            <card class="w-1/2" v-bind="maisons" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import card from "../components/card.vue";
+
+const maisons = [{ nom: "test", price: 750, favoris: true, txt: "Ecosse", nbbath: 2, nbsize: "test m²", img: "/public/maison.jpg" }, { nom: "test2", price: 590, favoris: false, txt: "Angleterre", nbbath: 2, nbsize: "test*2 m²", img: "/public/maison2.jpg" }, { nom: "Canada", price: 850, txt: "Dernier produit de test", nbbath: 1, nbsize: "test m²", img: "/public/maison3.jpg" }]
+//   import maisons from "../assets/maisons.json";
+</script>
