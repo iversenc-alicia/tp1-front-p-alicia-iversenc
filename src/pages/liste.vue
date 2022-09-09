52c07168f76d73444bfd2c8adb88020666219748
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-09-09 11:59:32 +0200

    liste

--- src/pages/liste.vue
+++ src/pages/liste.vue
@@ -10,6 +10,6 @@
 <script setup lang="ts">
 import card from "../components/card.vue";
 
-const maisons = [{ nom: "test", price: 750, favoris: true, txt: "Ecosse", nbbath: 2, nbsize: "test m²", img: "/public/maison.jpg" }, { nom: "test2", price: 590, favoris: false, txt: "Angleterre", nbbath: 2, nbsize: "test*2 m²", img: "/public/maison2.jpg" }, { nom: "Canada", price: 850, txt: "Dernier produit de test", nbbath: 1, nbsize: "test m²", img: "/public/maison3.jpg" }]
+const maisons = [{ nom: "test", price: 750, favoris: true, txt: "Ecosse", nbbath: 5, nbsize: "48 m²", img: "/public/maison.jpg" }, { nom: "test2", price: 590, favoris: false, txt: "Angleterre", nbbath: 2, nbsize: "84 m²", img: "/public/maison2.jpg" }, { nom: "Canada", price: 850, txt: "Dernier produit de test", nbbath: 1, nbsize: "52 m²", img: "/public/maison3.jpg" }]
 //   import maisons from "../assets/maisons.json";
 </script>
