5cea26c7d39132ed2d0575fefb61250b2e9ec4c3
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-09-09 11:30:35 +0200

    creation page simple

---
+++ src/pages/simple.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="p-4">
+        <h1 class="text-2xl">Page Simple</h1>
+        <card class="w-1/2" v-bind="objet" />
+    </div>
+</template>
+    
+<script setup lang="ts">
+import card from "../components/card.vue";
+
+const objet = { nom: "Maison sur la coline", price: 750, favoris: true, txt: "Ecosse", nbbath: 3, nbsize: "450 m²", img: "/public/maison.jpg" }
+</script>
