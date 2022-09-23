789357f6a9b48c4567568b05b095952eaf16c40a
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-09-23 19:26:08 +0200

    Formulaire version 2

--- src/components/FormulaireOffreMaison.vue
+++ src/components/FormulaireOffreMaison.vue
@@ -2,13 +2,32 @@
 import { ref } from "@vue/reactivity";
 import Card from "./card.vue";
 import { supabase } from '@/supabase';
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const maison = ref({});
+
+const props = defineProps(["id"]);
+if (props.id) {
+    // On charge les données de la maison
+    let { data, error } = await supabase
+        .from("Maison")
+        .select("*")
+        .eq("id", props.id);
+    if (error) console.log("n'a pas pu charger le table Maison :", error);
+    else maison.value = (data as any[])[0];
+}
 
 
-const maison = ref({ nom: "Adresse", price: 500, txt: "Lieu", nbbath: 5, nbsize: "200m2", img: "/maison.jpg" });
+
 //, favoris: true
 async function upsertMaison(dataForm, node) {
     const { data, error } = await supabase.from("Maison").upsert(dataForm);
     if (error || !data) node.setErrors([error?.message])
+    else {
+        node.setErrors([]);
+        router.push({ name: "edit-id", params: { id: data[0].id } });
+    }
 }
 
 
@@ -29,6 +48,10 @@ async function upsertMaison(dataForm, node) {
             }" :submit-attrs="{ classes: { input: 'bg-red-500 p-2 rounded' } }">
                 <FormKit name="nom" label="Lieu de la maison" />
                 <FormKit name="price" label="Le prix de la maison" type="number" />
+                <FormKit name="txt" label="L'adresse de la maison'" />
+                <FormKit name="nbbath" label="Nombre de salle de bain" />
+                <FormKit name="nbsize" label="Surface" />
+                <FormKit name="nbbed" label="Nombre de chambres" />
                 <!-- <FormKit name="favoris" label="mettre en valeur" type="checkbox" wrapper-class="flex" /> -->
             </FormKit>
         </div>
