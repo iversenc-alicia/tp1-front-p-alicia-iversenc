10e7d8a5b42b66c7532c0a60c93d59a245c5b509
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-09-23 17:55:02 +0200

    creation page ID

---
+++ src/pages/edit/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import FormulaireOffreMaison from "../../components/FormulaireOffreMaison.vue";
+defineProps({
+    id: String,
+})
+
+</script>
+
+<template>
+    <div>
+        <FormulaireOffreMaison :id="id" />
+    </div>
+</template>
