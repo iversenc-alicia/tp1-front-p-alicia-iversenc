6c4514640e8f984f7f8b5e9208f9b6c13e5cf687
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-09-23 17:50:03 +0200

    insérer de nouvelles offres (Version1)

--- src/components/FormulaireOffreMaison.vue
+++ src/components/FormulaireOffreMaison.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
 import { ref } from "@vue/reactivity";
-import Card from "./card.vue"
-const maison = ref({ nom: "Adresse", price: 500, favoris: true, txt: "Lieu", nbbath: 5, nbsize: "200m2", img: "/maison.jpg" });
+import Card from "./card.vue";
+import { supabase } from '@/supabase';
+
+
+const maison = ref({ nom: "Adresse", price: 500, txt: "Lieu", nbbath: 5, nbsize: "200m2", img: "/maison.jpg" });
+//, favoris: true
+async function upsertMaison(dataForm, node) {
+    const { data, error } = await supabase.from("Maison").upsert(dataForm);
+    if (error || !data) node.setErrors([error?.message])
+}
+
+
+
 </script>
 <template>
-    <div>
+    <div class="flex flex-row-reverse justify-center">
         <div class="p-2">
             <h2 class="text-2xl">Résultat (Prévisualisation)</h2>
             <Card class="w-3/4 m-auto" v-bind="maison" />
         </div>
         <div class="p-5">
-            <FormKit type="form" v-model="maison " :config="{
+            <FormKit @submit="upsertMaison" type="form" v-model="maison " :config="{
             classes: {
             input: 'p-1 rounded border-gray-400 shadow-m border',
             label: 'text-gray-800',
@@ -18,7 +29,7 @@ const maison = ref({ nom: "Adresse", price: 500, favoris: true, txt: "Lieu", nbb
             }" :submit-attrs="{ classes: { input: 'bg-red-500 p-2 rounded' } }">
                 <FormKit name="nom" label="Lieu de la maison" />
                 <FormKit name="price" label="Le prix de la maison" type="number" />
-                <FormKit name="favoris" label="mettre en valeur" type="checkbox" wrapper-class="flex" />
+                <!-- <FormKit name="favoris" label="mettre en valeur" type="checkbox" wrapper-class="flex" /> -->
             </FormKit>
         </div>
     </div>
