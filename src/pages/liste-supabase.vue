830b4941670593559e6533c60119847d76affd1c
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-09-23 13:13:56 +0200

    liste supabase fonctionnelle sauf img

--- src/pages/liste-supabase.vue
+++ src/pages/liste-supabase.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="p-2">
         <h1 class="text-2xl">Page Liste</h1>
-        <div v-for="maison in tableauMaisons" :key="maison.nom">
-            <card class="w-1/2" v-bind="maison" />
+        <div v-for="Maison in tableauMaisons" :key="Maison.nom">
+            <Card v-bind="Maison" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import card from "../components/card.vue";
+import { supabase } from "../supabase";
+import Card from "../components/card.vue";
+console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
 
-const res = await fetch("tableauMaisons.json");
-const tableauMaisons = await res.json();
+const maison = [supabase];
 
+const { data: tableauMaisons, error } = await supabase
+    .from('Maison')
+    .select('*')
+
+console.log("Maison:", maison);
 </script>
